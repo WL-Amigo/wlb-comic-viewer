fdb8b1dcb4a077b817ec8e52d5417321f515cdc7
Amigo (WhiteLuckBringers) <white.luckers@gmail.com>
2022-10-15 16:57:45 +0900

    feature: add virtualization to page list tab

--- core/ui/src/pages/lib/[libraryId]/book/[bookId]/components/AllPagesTabContent/index.tsx
+++ core/ui/src/pages/lib/[libraryId]/book/[bookId]/components/AllPagesTabContent/index.tsx
@@ -1,4 +1,5 @@
-import { Component, For } from 'solid-js';
+import { createVirtualizer, VirtualItem } from '@tanstack/solid-virtual';
+import { Component, createEffect, createSignal, For } from 'solid-js';
 import { BookOpenIcon } from '../../../../../../../component/Icons';
 import { useBookDataContext } from '../../Context';
 
@@ -7,20 +8,54 @@ interface Props {
 }
 export const AllPagesTabContent: Component<Props> = (props) => {
   const bookCtx = useBookDataContext();
+  const [containerRef, setContainerRef] = createSignal<HTMLDivElement | null>(null);
+  const rowVirtualizer = createVirtualizer({
+    get count() {
+      return bookCtx.book().pages.length;
+    },
+    getScrollElement: containerRef,
+    estimateSize: () => 42,
+  });
 
   return (
-    <div class="w-full h-full overflow-y-auto p-2 flex flex-col gap-y-2">
-      <For each={bookCtx.book().pages}>
-        {(pageName) => (
-          <button
-            class="rounded border px-2 py-1 flex flex-row gap-x-1 items-center hover:bg-gray-100"
-            onClick={() => props.onPageOpenRequested(pageName)}
-          >
-            <BookOpenIcon />
-            <span>{pageName}</span>
-          </button>
-        )}
-      </For>
+    <div ref={setContainerRef} class="w-full h-full overflow-y-auto px-2">
+      <div class="flex flex-col relative w-full" style={{ height: `${rowVirtualizer.getTotalSize()}px` }}>
+        <For each={rowVirtualizer.getVirtualItems()}>
+          {(vi) => (
+            <PageButton
+              virtualItem={vi}
+              pageName={bookCtx.book().pages[vi.index]}
+              onPageOpenRequested={props.onPageOpenRequested}
+            />
+          )}
+        </For>
+      </div>
+    </div>
+  );
+};
+
+interface ButtonProps {
+  pageName: string;
+  onPageOpenRequested: (pageName: string) => void;
+  virtualItem: VirtualItem<unknown>;
+}
+const PageButton: Component<ButtonProps> = (props) => {
+  return (
+    <div
+      class="absolute top-0 left-0 w-full py-1"
+      style={{
+        height: `${props.virtualItem.size}px`,
+        transform: `translateY(${props.virtualItem.start}px)`,
+      }}
+    >
+      <button
+        type="button"
+        class="rounded border px-2 py-1 flex flex-row gap-x-1 items-center hover:bg-gray-100 w-full"
+        onClick={() => props.onPageOpenRequested(props.pageName)}
+      >
+        <BookOpenIcon />
+        <span>{props.pageName}</span>
+      </button>
     </div>
   );
 };
